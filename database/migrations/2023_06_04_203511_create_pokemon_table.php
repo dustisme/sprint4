b10ef734629b35e176151a7355431e268a299515
dustisme <androsrivas@outlook.com>
2023-06-05 14:00:45 +0200

    pokemon model, controller and migration updated

--- database/migrations/2023_06_04_203511_create_pokemon_table.php
+++ database/migrations/2023_06_04_203511_create_pokemon_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\PokemonType;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -16,7 +17,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('code');
             $table->integer('level');
-            $table->integer('type_id');
+            $table->foreignIdFor(PokemonType::class)->unique();
             $table->timestamps();
         });
     }
