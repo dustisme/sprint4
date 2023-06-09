4b6b624b6c5a9f93a2135e69a6b8af9211f7e69a
dustisme <androsrivas@outlook.com>
2023-06-09 20:49:14 +0200

    cancel button updated

--- resources/views/trainers/create.blade.php
+++ resources/views/trainers/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <x-container>
 
-        <x-form action="/submitForm" class="p-4">
+        <x-form action="/trainers" class="p-4">
 
             <x-section>
                 <h2 class="text-white font-bold">REGISTER POKÉMON TRAINER</h2>
@@ -15,10 +15,6 @@
                     <label for="trainer_name" class="text-white font-bold">TRAINER'S NAME <span
                             class="text-red-400 text-xs">&#10033;</span></label><br>
                     <input id="trainer_name" name="trainer_name" type="text" class="rounded"><br>
-                    @error('name')
-                        <small class="text-red-500">*{{ $message }}</small>
-                        <br>
-                    @enderror
                 </div>
                 <br>
 
@@ -26,10 +22,6 @@
                     <label for="trainer_code" class="text-white font-bold">TRAINER'S CODE <span
                             class="text-red-400 text-xs">&#10033;</span></label><br>
                     <input id="trainer_code" name="trainer_code" type="text" class="rounded"><br>
-                    @error('code')
-                        <small class="text-red-500">*{{ $message }}</small>
-                        <br>
-                    @enderror
                 </div>
                 <br>
                 {{-- Pokemon input field --}}
@@ -37,10 +29,6 @@
                     <label for="pokemon_name" class="text-white font-bold">POKÉMON NAME <span
                             class="text-red-400 text-xs">&#10033;</span></label><br>
                     <input id="pokemon_name" name="pokemon_name" type="text" class="rounded"><br>
-                    @error('pokemon_name')
-                        <small class="text-red-500">*{{ $message }}</small>
-                        <br>
-                    @enderror
                 </div>
                 <br>
 
@@ -49,10 +37,6 @@
                             class="text-red-400 text-xs">&#10033;</span></label><br>
                     <input id="pokemon_level" name="pokemon_level" type="number" class="rounded" min="1"
                         max="100"><br>
-                    @error('pokemon_level')
-                        <small class="text-red-500">*{{ $message }}</small>
-                        <br>
-                    @enderror
                 </div>
                 <br>
                 {{-- PokemonType input field --}}
@@ -61,14 +45,10 @@
                             class="text-red-400 text-xs">&#10033; Select
                             One</span></label><br>
                     <select name="pokemon_type" id="pokemon_type" class="rounded"><br>
-                        @foreach ($pokemonTypes as $pokemonType)
-                            <option value="{{ $pokemonType->id }}">{{ $pokemonType->name }}</option>
+                        @foreach ($types as $type)
+                            <option value="{{ $type->id }}">{{ $type->name }}</option>
                         @endforeach
                     </select>
-                    @error('pokemon_type')
-                        <small class="text-red-500">*{{ $message }}</small>
-                        <br>
-                    @enderror
                 </div>
                 <br>
                 <div class="flex justify-evenly">
@@ -78,13 +58,11 @@
                     <x-button type="reset" class="bg-slate-500 p-2 rounded">
                         RESET
                     </x-button>
-                    <x-form action="/trainers.index">
-                        <x-button class="bg-red-400 hover:bg-red-500 p-2 rounded">
-                            CANCEL
-                        </x-button>
-                    </x-form>
+                    <a href="/trainers"
+                        class="bg-red-400 hover:bg-red-500  hover:text-white inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs uppercase tracking-widest focus:outline-none focus:ring-1 focus:ring-slate-50 focus:ring-offset-1 transition ease-in-out duration-150">
+                        CANCEL
+                    </a>
                 </div>
-
             </x-section>
 
         </x-form>
